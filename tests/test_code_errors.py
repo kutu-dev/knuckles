de8685802542214b99048f389fe70d722d931072
Kutu <the.kutu.dev@gmail.com>
2023-07-22 15:58:13 +0200

    Change tests to avoid code repeting in code error tests

--- tests/test_code_errors.py
+++ tests/test_code_errors.py
@@ -1,12 +1,55 @@
-import knuckles
+import knuckles.exceptions
 import pytest
 import responses
 from knuckles import Subsonic
 from responses import matchers
 
+code_errors = [
+    (0, "A generic error.", knuckles.exceptions.CodeError0),
+    (10, "Required parameter is missing.", knuckles.exceptions.CodeError10),
+    (
+        20,
+        "Incompatible Subsonic REST protocol version. Client must upgrade.",
+        knuckles.exceptions.CodeError20,
+    ),
+    (
+        30,
+        "Incompatible Subsonic REST protocol version. Server must upgrade.",
+        knuckles.exceptions.CodeError30,
+    ),
+    (40, "Wrong username or password.", knuckles.exceptions.CodeError40),
+    (
+        41,
+        "Token authentication not supported for LDAP users.",
+        knuckles.exceptions.CodeError41,
+    ),
+    (
+        50,
+        "User is not authorized for the given operation.",
+        knuckles.exceptions.CodeError50,
+    ),
+    (
+        60,
+        (
+            "The trial period for the Subsonic server is over."
+            + "Please upgrade to Subsonic Premium. Visit subsonic.org for details."
+        ),
+        knuckles.exceptions.CodeError60,
+    ),
+    (70, "The requested data was not found.", knuckles.exceptions.CodeError70),
+    (80, "The cake is a lie!", knuckles.exceptions.UnknownErrorCode),
+]
 
+
+@pytest.mark.parametrize("code, message, exception", code_errors)
 @responses.activate
-def test_code_error_0(subsonic: Subsonic, params: dict[str, str]) -> None:
+def test_code_errors(
+    subsonic: Subsonic,
+    params: dict[str, str],
+    code: int,
+    message: str,
+    exception,
+) -> None:
     responses.add(
         responses.GET,
         url="https://example.com/rest/ping",
@@ -18,273 +61,11 @@ def test_code_error_0(subsonic: Subsonic, params: dict[str, str]) -> None:
                 "type": "knuckles",
                 "serverVersion": "0.1.3 (tag)",
                 "openSubsonic": True,
-                "error": {"code": 0, "message": "A generic error."},
+                "error": {"code": code, "message": message},
             }
         },
         status=200,
     )
 
-    with pytest.raises(knuckles.exceptions.CodeError0, match="A generic error."):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_10(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {"code": 10, "message": "Required parameter is missing."},
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError10, match="Required parameter is missing."
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_20(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 20,
-                    "message": (
-                        "Incompatible Subsonic REST protocol version.",
-                        "Client must upgrade.",
-                    ),
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError20,
-        match="Incompatible Subsonic REST protocol version. Client must upgrade.",
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_30(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 30,
-                    "message": (
-                        "Incompatible Subsonic REST protocol version."
-                        + "Server must upgrade."
-                    ),
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError30,
-        match="Incompatible Subsonic REST protocol version. Server must upgrade.",
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_40(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 40,
-                    "message": "Wrong username or password.",
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError40,
-        match="Wrong username or password.",
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_41(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 41,
-                    "message": "Token authentication not supported for LDAP users.",
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError41,
-        match="Token authentication not supported for LDAP users.",
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_50(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 50,
-                    "message": "User is not authorized for the given operation.",
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError50,
-        match="User is not authorized for the given operation.",
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_60(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 60,
-                    "message": (
-                        "The trial period for the Subsonic server is over."
-                        + "Please upgrade to Subsonic Premium."
-                        + "Visit subsonic.org for details."
-                    ),
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError60,
-        match=(
-            "The trial period for the Subsonic server is over."
-            + "Please upgrade to Subsonic Premium."
-            + "Visit subsonic.org for details."
-        ),
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_code_error_70(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 70,
-                    "message": "The requested data was not found.",
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.CodeError70,
-        match="The requested data was not found.",
-    ):
-        subsonic.ping()
-
-
-@responses.activate
-def test_unknown_code_error(subsonic: Subsonic) -> None:
-    responses.add(
-        responses.GET,
-        "https://example.com/rest/ping",
-        json={
-            "subsonic-response": {
-                "status": "failed",
-                "version": "1.16.1",
-                "type": "knuckles",
-                "serverVersion": "0.1.3 (tag)",
-                "openSubsonic": True,
-                "error": {
-                    "code": 80,
-                    "message": "The cake is a lie!",
-                },
-            }
-        },
-        status=200,
-    )
-
-    with pytest.raises(
-        knuckles.exceptions.UnknownErrorCode,
-        match="The cake is a lie!",
-    ):
+    with pytest.raises(exception, match=message):
         subsonic.ping()
