69d6713d5225b7ee593637b2a8a754d67e5f32c3
Kutu <the.kutu.dev@gmail.com>
2023-07-26 03:46:19 +0200

    Clean __init__.py in the main package

--- src/knuckles/__init__.py
+++ src/knuckles/__init__.py
@@ -21,17 +21,3 @@ __all__ = [
     "Song",
     "SubsonicResponse",
 ]
-
-# TODO to implement
-# * - setRating -> Album(Non ID3), Artist(Non ID3)
-# * - scrobble -> Video -> (Multiple files at the same time? ambiguous docs)
-
-#! Should be implemented
-# * - POST Support
-
-#! Not implemented
-# * - Audio transcoding
-# * - Video
-# * - Open Subsonic Extensions
-# * - CA Certs
-# * - Non ID3
